d2251822436b07b5180b6c05c3fcdf9c55f14541
SoshinK <soshinkonstantinv@gmail.com>
2021-03-24 17:36:58 +0300

    ADD: added open3d visualisation and jbu script

---
+++ jbu.py
@@ -0,0 +1,119 @@
+import argparse
+import matplotlib.pyplot as plt
+from PIL import Image
+import numpy as np
+from concurrent.futures import ProcessPoolExecutor
+import cv2 as cv2 
+# parser = argparse.ArgumentParser(description="Perform Joint Bilateral Upsampling with a source and reference image")
+# parser.add_argument("source", help="Path to the source image")
+# parser.add_argument("reference", help="Path to the reference image")
+# parser.add_argument("output", help="Path to the output image")
+# parser.add_argument('--radius', dest='radius', default=2, help='Radius of the filter kernels (default: 2)')
+# parser.add_argument('--sigma-spatial', dest='sigma_spatial', default=2.5, help='Sigma of the spatial weights (default: 2.5)')
+# parser.add_argument('--sigma-range', dest='sigma_range', help='Sigma of the range weights (default: standard deviation of the reference image)')
+# args = parser.parse_args()
+
+# img_source = 'depth0592.png'
+# img_ref = 'color0592.png'
+# output = 'output.png'
+img_source = 'depth_transformed.png'
+img_ref = "color_undist2.png"
+output = 'output_aligned.png'
+args_radius = 2
+args_sigma_spatial = 1.5
+args_sigma_range = None
+
+source_image = Image.open(img_source)
+
+reference_image = Image.open(img_ref)
+reference = np.array(reference_image)
+
+source_image_upsampled = source_image.resize(reference_image.size, Image.BILINEAR)
+# plt.imshow(source_image_upsampled)
+# plt.show()
+source_upsampled = np.array(source_image_upsampled)
+source_upsampled = np.array([source_upsampled, source_upsampled, source_upsampled]).transpose((1, 2, 0))
+print(source_upsampled.shape)
+scale = source_image.width / reference_image.width
+radius = int(args_radius)
+diameter = 2 * radius + 1
+step = int(np.ceil(1 / scale))
+padding = radius * step
+sigma_spatial = float(args_sigma_spatial)
+sigma_range = float(args_sigma_range) if args_sigma_range else np.std(reference)
+print("?", reference.shape)
+reference = np.pad(reference, ((padding, padding), (padding, padding), (0, 0)), 'symmetric').astype(np.float32)
+source_upsampled = np.pad(source_upsampled, ((padding, padding), (padding, padding), (0, 0)), 'symmetric').astype(np.float32)
+
+# Spatial Gaussian function.
+x, y = np.meshgrid(np.arange(diameter) - radius, np.arange(diameter) - radius)
+kernel_spatial = np.exp(-1.0 * (x**2 + y**2) /  (2 * sigma_spatial**2))
+kernel_spatial = np.repeat(kernel_spatial, 3).reshape(-1, 3)
+
+# Lookup table for range kernel.
+lut_range = np.exp(-1.0 * np.arange(256)**2 / (2 * sigma_range**2))
+
+def process_row(y):
+   result = np.zeros((reference_image.width, 3))
+   y += padding
+   for x in range(padding, reference.shape[1] - padding):
+      I_p = reference[y, x]
+      patch_reference = reference[y - padding:y + padding + 1:step, x - padding:x + padding + 1:step].reshape(-1, 3)
+      patch_source_upsampled = source_upsampled[y - padding:y + padding + 1:step, x - padding:x + padding + 1:step].reshape(-1, 3)
+      patch_source_upsampled_mask = patch_source_upsampled != 0
+      # print("!", patch_source_upsampled)
+      kernel_range = lut_range[np.abs(patch_reference - I_p).astype(int)]
+      weight = kernel_range * kernel_spatial
+      # print("?", weight[patch_source_upsampled_mask])
+      # if np.any(patch_source_upsampled_mask):
+      #    print("yup!")
+      #    print(">", patch_source_upsampled)
+      #    print(">>", patch_source_upsampled_mask)
+      #    print(">>>", patch_source_upsampled[patch_source_upsampled_mask])
+      # print(patch_source_upsampled.shape, weight.shape, patch_source_upsampled[patch_source_upsampled_mask].shape, patch_source_upsampled_mask.shape)
+      k_p = weight[patch_source_upsampled_mask].sum(axis=0)
+      if k_p == 0:
+         k_p = 1
+      # print(weight)
+      # exit()
+      result[x - padding] = np.round(np.sum(weight[patch_source_upsampled_mask] * patch_source_upsampled[patch_source_upsampled_mask], axis=0) / k_p)
+
+   return result
+
+executor = ProcessPoolExecutor(max_workers=8)
+result = executor.map(process_row, range(reference_image.height))
+executor.shutdown(True)
+# Image.fromarray(np.array(list(result)).astype(np.uint8)).save(output)
+result = np.array(list(result))
+result = result / np.amax(result) * np.amax(source_image)
+# print(result.shape, np.amax(result))
+# print(result[0, 0], type(result[0, 0, 0]))
+result = np.uint16(result)[:, :, 0]
+# print(result.shape, type(result[0, 0]))
+
+# plt.figure(figsize=(8,16))
+# plt.imshow(source_image)
+# plt.show()
+
+# plt.figure(figsize=(8,16))
+# plt.imshow(result)
+# plt.show()
+
+plt.figure(figsize=(16,16))
+plt.subplot(121)
+plt.imshow(source_image)
+plt.title('Reprojected depth map')
+# plt.colorbar()
+plt.subplot(122)
+plt.imshow(result)
+print(type(result[0, 0]))
+
+plt.title(f'Joint bilateral filtering (sigma_spatial={args_sigma_spatial}, sigma_range={sigma_range})')
+# plt.colorbar()
+
+plt.show()
+
+# plt.imshow(source_image - result)
+# plt.show()
+
+cv2.imwrite(output, result)
---
+++ open3d_vis.py
@@ -0,0 +1,63 @@
+import matplotlib.pyplot as plt
+import numpy as np
+import open3d as o3d
+import io
+import cv2
+import json
+
+# img = cv2.imread('/home/kvsoshin/Kinect Telepresence/depth0592.png', -1)
+img = cv2.imread('output_aligned.png', -1)
+# img = cv2.imread('/home/kvsoshin/Kinect Telepresence/output_aligned6.png', -1)
+color = cv2.imread('001236.jpg', -1).astype(np.uint16)
+color = cv2.cvtColor(color, cv2.COLOR_BGR2RGB)
+
+# color_raw = o3d.io.read_image("/home/kvsoshin/Kinect Telepresence/output_aligned2.png")
+color_raw = o3d.geometry.Image(color.astype(np.uint8))
+# depth_raw = o3d.io.read_image("/home/kvsoshin/Kinect Telepresence/color0592.png")
+depth_raw = o3d.geometry.Image(img.astype(np.uint16))
+print(color_raw)
+print(depth_raw)
+rgbd_image = o3d.geometry.RGBDImage.create_from_color_and_depth(color_raw, depth_raw, depth_scale=1000, convert_rgb_to_intensity=False, depth_trunc=10.0)
+
+
+
+with open('mkv_meta.json') as f:
+    intrinsic_params = json.load(f)
+print(np.amax(img), np.amin(img))
+print(o3d.__version__)
+camera_intrinsic_params = {}
+tmp = intrinsic_params['rgb_undistorted_intrinsics'].copy()
+tmp.update(intrinsic_params['color_resolution'].copy())
+# tmp = intrinsic_params['depth_intrinsics'].copy()
+# tmp.update(intrinsic_params['depth_resolution'].copy())
+camera_intrinsic_params['height'] = tmp['h']
+camera_intrinsic_params['width'] = tmp['w']
+camera_intrinsic_params['fx'] = tmp['fx']
+camera_intrinsic_params['fy'] = tmp['fy']
+camera_intrinsic_params['cx'] = tmp['cx']
+camera_intrinsic_params['cy'] = tmp['cy']
+
+print(camera_intrinsic_params)
+
+# o3d_depth = o3d.geometry.Image(img)
+# camera_intrinsic = o3d.camera.PinholeCameraIntrinsic(
+#     o3d.camera.PinholeCameraIntrinsicParameters.Kinect2ColorCameraDefault)
+# camera_intrinsic = o3d.io.read_pinhole_camera_intrinsic('data/mkv_meta.json')
+camera_intrinsic = o3d.camera.PinholeCameraIntrinsic(**camera_intrinsic_params)
+
+
+
+# pcd = o3d.geometry.PointCloud.create_from_depth_image(o3d_depth, camera_intrinsic)
+# rgbd = o3d.geometry.RGBDImage.create_from_color_and_depth(color, img)
+pcd = o3d.geometry.PointCloud.create_from_rgbd_image(rgbd_image, camera_intrinsic)
+
+
+
+o3d.io.write_point_cloud('depth.ply', pcd, print_progress=True)
+
+pcd.transform([[1, 0, 0, 0], [0, -1, 0, 0], [0, 0, -1, 0], [0, 0, 0, 1]])
+
+o3d.visualization.draw_geometries([pcd])
+
+plt.imshow(np.array(rgbd_image.depth))
+plt.show()
